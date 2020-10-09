c0a784099277242094ebd2a18df558aad4b987c3
ErgoAsciiCat <72501990+ErgoAsciiCat@users.noreply.github.com>
2020-10-09 17:06:44 +0200

    ovn8
    basic vector methods and prep for lab 8

---
+++ ovn8/src/TrainingVector.java
@@ -0,0 +1,48 @@
+public class TrainingVector {
+	public int[] v;
+	public TrainingVector (int i) {
+		this.v = new int[i];
+	}
+	
+	public int[] add (int value, int index) {
+		int[] newArray = new int [v.length + 1];
+		for (int i = 0, k = 0; i < newArray.length; i++) {
+			if (i == index) {
+				newArray[i] = value;
+			} else {
+					newArray[i]=v[k++];
+			}
+		}
+		return newArray;
+	}
+	
+	public int[] remove (int index) {
+		int[] newArray = new int [v.length - 1];
+		for (int i = 0, k = 0; i < v.length; i++) {
+			if (i == index) {
+				//System.out.println("Index: " + i + " K= " + k + " Bokstav " + (char)v[i]);
+				continue;
+				
+			} else {
+				//System.out.println("Index: " + i + " K= " + k);
+					newArray[k++]=v[i];
+				
+			}
+		}
+		return newArray;
+	}
+
+	public boolean checkForPositives(){
+		for (int i : v) {
+			if (i <= 64) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public int length(){
+		return v.length;
+	}
+}
+
